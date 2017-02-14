b07271942a2b1b46d5a29e8a5abf854f250d3d72
Jan-Philippe Lavoie <jan-philippe.lavoie@ixiasoft.com>
2017-02-14 15:52:56 -0500

    Initial commit - working restful backend

---
+++ proto/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config
+db = SQLAlchemy(app)
+
+import proto.views
+import proto.models
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='proto',
+    packages=['proto'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
