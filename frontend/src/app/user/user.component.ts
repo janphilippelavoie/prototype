1c56adf438434ea87eb6de8b83a51e0178432038
Jan-Philippe Lavoie <janphilippelavoie@gmail.com>
2017-02-18 00:24:17 -0500

    user-list component example now uses a user component

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  @Input() user: User;
+  
   constructor() { }
 
   ngOnInit() {
