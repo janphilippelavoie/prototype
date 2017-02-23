1ed33ccc5f5806743da3ac25598678bc1257c379
Jan-Philippe Lavoie <jan-philippe.lavoie@ixiasoft.com>
2017-02-23 17:29:34 -0500

    WIP: client side authentication

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Headers, Http, RequestOptions, Response } from '@angular/http';
-import { Component, Injectable, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { AuthHttp, JwtHelper } from 'angular2-jwt';
-import { User } from '../user/user.model';
+import {Headers, Http, RequestOptions, Response} from '@angular/http';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
+import {AuthHttp, JwtHelper} from 'angular2-jwt';
+import {User} from '../user/user.model';
 
 
 @Component({
@@ -13,48 +13,46 @@ import { User } from '../user/user.model';
 @Injectable()
 export class LoginComponent implements OnInit {
 
-private messages: String[] = [];
+  private messages: String[] = [];
 
- private APP_SERVER: String = "http://localhost:5000/";
- private username: String = "jp@example.com";
- private password: String = "password";
+  private APP_SERVER: String = "http://localhost:5000/";
+  private username: String = "jp@example";
+  private password: String = "password";
 
- public loginForm = this.formBuilder.group({
+  public loginForm: FormGroup = this.formBuilder.group({
     username: ["", Validators.required],
     password: ["", Validators.required],
   });
 
   user: User;
 
-  constructor(public formBuilder: FormBuilder, private http: Http) { }
+  constructor(public formBuilder: FormBuilder, private http: Http) {
+  }
 
   doLogin(event) {
     let options: RequestOptions = new RequestOptions({
-      headers: new Headers({ 'Content-Type': 'application/json' })
+      headers: new Headers({'Content-Type': 'application/json'})
     });
-    console.log("Form: " + this.loginForm.value);
+    console.log("Form: " + this.loginForm.controls['username'].value);
     this.http.post(
       this.APP_SERVER + 'auth',
-      JSON.stringify({ "username": "jp@example.com", "password": "password" }),
-      options
-      ).map((response: Response) => response.json)
-        .subscribe(
-                     (data: any) => {console.log("data" + JSON.stringify(data)),
-                     err => console.log("err: " + err),
-                     ()  => console.log("what is dat")
+      JSON.stringify({"username": this.username, "password": this.password}),
+      options)
+      .map((response: Response) => response.json())
+      .subscribe((data: any) => {
 
-          // save the token in local storage
-          // let token: String = data.access_token;
-          // this.messages.push(token)
-          // localStorage.setItem('id_token', token);
-          // this.messages.push(`Login successful, token saved.`);
-  
-          // let jwtHelper: JwtHelper = new JwtHelper();
-          // this.messages.push(`expiration: ${jwtHelper.getTokenExpirationDate(token)}`);
-          // this.messages.push(`is expired: ${jwtHelper.isTokenExpired(token)}`);
-          // this.messages.push(`decoded: ${JSON.stringify(jwtHelper.decodeToken(token))}`);
-                     }
-             );
+        // save the token in local storage
+        let token: string = data.access_token;
+        this.messages.push(token)
+        localStorage.setItem('id_token', token);
+        this.messages.push(`Login successful, token saved.`);
+
+        let jwtHelper: JwtHelper = new JwtHelper();
+        this.messages.push(`expiration: ${jwtHelper.getTokenExpirationDate(token)}`);
+        this.messages.push(`is expired: ${jwtHelper.isTokenExpired(token)}`);
+        this.messages.push(`decoded: ${JSON.stringify(jwtHelper.decodeToken(token))}`);
+      });
+
 
     //   JSON.stringify({ 'username': this.email, 'password': PASSWORD }),
     //   options)
@@ -65,12 +63,12 @@ private messages: String[] = [];
     //     let token = data.access_token;
     //     localStorage.setItem('id_token', token);
     //     this.messages.push(`Login successful, token saved.`);
- 
+
     //     let jwtHelper: JwtHelper = new JwtHelper();
     //     this.messages.push(`expiration: ${jwtHelper.getTokenExpirationDate(token)}`);
     //     this.messages.push(`is expired: ${jwtHelper.isTokenExpired(token)}`);
     //     this.messages.push(`decoded: ${JSON.stringify(jwtHelper.decodeToken(token))}`);
- 
+
     //     // now get the protected resource
     //     this.getProtected();
     //   },
@@ -78,7 +76,8 @@ private messages: String[] = [];
     //     this.messages.push(`Login failed: ${error}`);
     //   }
     //   );
-  }    
+  }
+
 
 
   ngOnInit() {
